2fd26d41ce6a9452da158683db2ec99b09863cb1
Anqi <1518014096@qq.com>
2021-07-05 00:00:40 +0800

    Test

--- src/components/DailyWork/DailyWork.vue
+++ src/components/DailyWork/DailyWork.vue
@@ -3,12 +3,14 @@
     <div class="add-area">
       <el-form inline>
         <el-form-item label="添加日程">
-          <el-input v-model="name" @keyup.enter.native="handleAddItem" clearable></el-input>
+          <el-input
+            v-model="name"
+            @keyup.enter.native="handleAddItem"
+            clearable
+          ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="handleAddItem">
-            添加
-          </el-button>
+          <el-button type="primary" @click="handleAddItem"> 添加 </el-button>
         </el-form-item>
       </el-form>
       <div>
@@ -24,11 +26,19 @@
           </div>
           <el-table :data="unDoneList" empty-text="任务都已经完成啦">
             <el-table-column label="编号" type="index"></el-table-column>
-            <el-table-column label="日程" prop="name" min-width="150px"></el-table-column>
+            <el-table-column
+              label="日程"
+              prop="name"
+              min-width="150px"
+            ></el-table-column>
             <el-table-column label="操作" min-width="150px">
-              <template v-slot="{row}">
-                <el-button type="primary" size="small" @click="handleDone(row)">已经完成</el-button>
-                <el-button type="danger" size="small" @click="handleRemove(row)">删除日程</el-button>
+              <template v-slot="{ row }">
+                <el-button type="primary" size="small" @click="handleDone(row)"
+                  >已经完成</el-button
+                >
+                <el-button type="danger" size="small" @click="handleRemove(row)"
+                  >删除日程</el-button
+                >
               </template>
             </el-table-column>
           </el-table>
@@ -41,11 +51,22 @@
           </div>
           <el-table :data="doneList" empty-text="加油把左边的项目都拖到这里">
             <el-table-column label="编号" type="index"></el-table-column>
-            <el-table-column label="日程" prop="name" min-width="150px"></el-table-column>
+            <el-table-column
+              label="日程"
+              prop="name"
+              min-width="150px"
+            ></el-table-column>
             <el-table-column label="操作" min-width="150px">
-              <template v-slot="{row}">
-                <el-button type="warning" size="small" @click="handleCancelDone(row)">撤销完成</el-button>
-                <el-button type="danger" size="small" @click="handleRemove(row)">删除日程</el-button>
+              <template v-slot="{ row }">
+                <el-button
+                  type="warning"
+                  size="small"
+                  @click="handleCancelDone(row)"
+                  >撤销完成</el-button
+                >
+                <el-button type="danger" size="small" @click="handleRemove(row)"
+                  >删除日程</el-button
+                >
               </template>
             </el-table-column>
           </el-table>
@@ -56,58 +77,53 @@
 </template>
 
 <script>
-import {mapState, mapGetters} from 'vuex'
+import { mapState, mapGetters } from "vuex";
 
 export default {
   name: "DailyWork",
   data() {
     return {
-      name: ''
-    }
+      name: "",
+    };
   },
   computed: {
-    ...mapState([]),
-    ...mapGetters(['unDoneList', 'doneList', 'currentMaxId'])
+    ...mapGetters(["unDoneList", "doneList", "currentMaxId"]),
   },
   methods: {
     handleAddItem() {
       if (this.name.trim().length === 0) {
         return this.$message({
-          type: 'warning',
-          message: '请填入日程名称'
-        })
+          type: "warning",
+          message: "请填入日程名称",
+        });
       }
       const newItem = {
         id: this.currentMaxId + 1,
         name: this.name,
-        done: false
-      }
-      this.$store.commit('addItemMutation', newItem)
-      this.name = ''
-    }
-    ,
+        done: false,
+      };
+      this.$store.commit("addItemMutation", newItem);
+      this.name = "";
+    },
     handleDone(row) {
-      this.$store.commit('doneMutation', row.id)
-    }
-    ,
+      this.$store.commit("doneMutation", row.id);
+    },
     handleRemove(row) {
-      this.$store.commit('removeMutation', row.id)
-    }
-    ,
+      this.$store.commit("removeMutation", row.id);
+    },
     handleCancelDone(row) {
-      this.$store.commit('cancelDone', row.id)
-    }
-    ,
+      this.$store.commit("cancelDone", row.id);
+    },
     async handleClearItem() {
       try {
-        await this.$confirm('确定清空日程吗？', '确定')
+        await this.$confirm("确定清空日程吗？", "确定");
       } catch (e) {
-        return
+        return;
       }
-      this.$store.commit('clearAll')
-    }
-  }
-}
+      this.$store.commit("clearAll");
+    },
+  },
+};
 </script>
 
 <style lang="less">
